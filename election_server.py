2bbf0a42357449466f03662301df9c9b7b68c33f
Kritanta <KritantaDevelopment@gmail.com>
2020-11-03 19:34:07 -0600

    Update election_server.py

--- election_server.py
+++ election_server.py
@@ -5,26 +5,10 @@ import requests
 from bs4 import BeautifulSoup
 import os, time, json, sys
 from collections import namedtuple 
-import asyncio
 Candidate = namedtuple("Candidate", ('name', 'electoral', 'percentage', 'count'))
 
-if sys.stdout.isatty() and 'noterm' not in sys.argv:
-    print("Running in terminal context, updating every 10 seconds on loop")
-
-
-from sanic import Sanic
-from sanic.response import json
-
-app = Sanic("App Name")
 result = "waiting for update"
-@app.route("/")
-async def test(request):
-    return json(result)
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
-
-async def looper():
+if True:
     while True:
         url = "https://www.google.com/search?q=us+election+results&rlz=1C1SQJL_enUS909US911&aqs=chrome.0.0i67i131i433i457j0i20i131i263i433j0i131i433j0i67j0i131i433l2j69i60l2.1612j0j9&sourceid=chrome&ie=UTF-8"
         headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
@@ -57,18 +41,13 @@ async def looper():
         ]
         
             print(json.dumps(fun))
-            result = fun
+            with open("/usr/share/nginx/html/index.html", "w") as output:
+                json.dump(fun, output)
     
         if requests.get("https://raw.githubusercontent.com/KritantaDev/election/main/update1").status_code == 200:
             if requests.get("https://raw.githubusercontent.com/KritantaDev/election/main/update1").text == 'update':
                 print("UPDATE AVAILABLE - REDOWNLOAD FROM https://github.com/kritantadev/election OR RUN git pull")
-        await asyncio.sleep(10)
+        time.sleep(10)
 
-async def main():
-    print('starting')
-    future = asyncio.ensure_future(looper())
-    print('waiting')
- 
-if __name__ == '__main__':
-    asyncio.get_event_loop().run_until_complete(main())
+
 
